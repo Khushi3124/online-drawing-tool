cf3b2cfc7a10516221165149f5102878515c0af3
Khushi <khushikachhap24@gmail.com>
2024-07-14 18:52:37 +0530

    Implemented pencil,eraser and many more tools in the toolbox section.

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,15 +1,18 @@
+import { useSelector } from 'react-redux'
 import styles from './index.module.css'
-import { COLORS } from '@/constants'
+import { COLORS, MENU_ITEMS } from '@/constants'
 
 const Toolbox = () => {
-
+    const activeMenuItem= useSelector((state)=>state.menu.activeMenuItem)
+    const showStrokeToolOption = activeMenuItem===MENU_ITEMS.PENCIL
+    const showBrushToolOption = activeMenuItem===MENU_ITEMS.PENCIL||activeMenuItem===MENU_ITEMS.ERASER
     const updateBrushSize = (e) => {
 
     }
     
     
     return (<div className={styles.toolboxContainer}>
-        <div className={styles.toolItem}>
+        {showStrokeToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Stroke Color</h4>
             <div className={styles.itemContainer}>
                 <div className={styles.colorBox} style={{backgroundColor: COLORS.BLACK}} onClick={() => updateColor(COLORS.BLACK)}/>
@@ -18,14 +21,15 @@ const Toolbox = () => {
                 <div className={styles.colorBox} style={{backgroundColor: COLORS.BLUE}} onClick={() => updateColor(COLORS.BLUE)}/>
                 <div className={styles.colorBox} style={{backgroundColor: COLORS.ORANGE}} onClick={() => updateColor(COLORS.ORANGE)}/>
                 <div className={styles.colorBox} style={{backgroundColor: COLORS.YELLOW}} onClick={() => updateColor(COLORS.YELLOW)}/>
+                <div className={styles.colorBox} style={{backgroundColor: COLORS.PURPLE}} onClick={() => updateColor(COLORS.PURPLE)}/>
             </div>
-        </div>
-        <div className={styles.toolItem}>
+        </div>}
+        {showBrushToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Brush Size</h4>
             <div className={styles.itemContainer}>
                 <input type="range" min={1} max={10} step={1} onChange={updateBrushSize}/>
             </div>
-        </div>
+        </div>}
     </div>)
 }
 
